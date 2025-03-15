67ff3a9701e5134b17a509a1ecd9ed9b552a6de6
Sam Perales <91507521+samuelperalesg@users.noreply.github.com>
2025-03-15 20:54:06 +0000

    3/15 ch 1

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 - adding comment
+// Exercise 00_03
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
